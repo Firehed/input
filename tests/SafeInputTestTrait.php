c88559208bcc53dd04d32ba585995b8763154250
Eric Stern <eric@ericstern.com>
2015-10-08 13:13:52 -0700

    Add SafeInputTestTrait
    Summary: Create a trait to be used by test cases for transforming arbitrary data into "safe" input. Expressly for the purpose of testing consumers of validated data where testing the validation process is unnecessary.
    
    Test Plan: This was copied from a tested codebase already using it
    
    Differential Revision: https://phabricator.ericstern.com/D69

---
+++ tests/SafeInputTestTrait.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Firehed\Input;
+
+trait SafeInputTestTrait
+{
+
+    /**
+     * Transform an array of data to a SafeInput object. Intended for testing
+     * consumers of validated input where testing the actual validation process
+     * is either unnecessary or undesirable.
+     *
+     * Depends on PHPUnit >=4.8
+     *
+     * @param array "valid" data
+     * @return Containers\SafeInput wrapped valid data
+     */
+    protected function getSafeInputFromData(array $data)
+    {
+        $PI = $this->getMockBuilder('Firehed\Input\Containers\ParsedInput')
+            ->setConstructorArgs([$data])
+            ->getMock();
+        $PI->expects($this->any())
+            ->method('isValidated')
+            ->will($this->returnValue(true));
+        return new Containers\SafeInput($PI);
+    }
+
+}
