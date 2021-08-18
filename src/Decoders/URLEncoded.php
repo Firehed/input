21413d9ceee8d08f280fd834de8654b6dfa401c8
Eric Stern <eric@ericstern.com>
2021-08-18 11:45:39 -0700

    Update URLEncoded decoder logic

--- src/Decoders/URLEncoded.php
+++ src/Decoders/URLEncoded.php
@@ -7,23 +7,28 @@ namespace Firehed\Input\Decoders;
 use Firehed\Input\Exceptions\InputException;
 use Firehed\Input\Interfaces\ParserInterface;
 
+use function is_array;
+use function parse_str;
+use function strlen;
+
 class URLEncoded implements ParserInterface, DecoderInterface
 {
     public function decode(string $rawInput): mixed
     {
-    }
-
-    public function parse(string $raw_input): array
-    {
-        if (!strlen($raw_input)) {
+        if (!strlen($rawInput)) {
             return [];
         }
         $out = [];
-        parse_str($raw_input, $out);
+        parse_str($rawInput, $out);
         if (!is_array($out) || !$out) {
             throw new InputException(InputException::FORMAT_ERROR);
         }
         return $out;
+    }
+
+    public function parse(string $raw_input): array
+    {
+        return $this->decode($raw_input);
     }
 
     public function getSupportedMimeTypes(): array
