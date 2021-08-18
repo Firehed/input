bfbb04cc49a4ea4c061f7a30e1460b7e9abe3368
Eric Stern <eric@ericstern.com>
2021-08-18 11:51:08 -0700

    Implement json decoder

--- src/Decoders/JSON.php
+++ src/Decoders/JSON.php
@@ -7,12 +7,32 @@ namespace Firehed\Input\Decoders;
 use Firehed\Input\Exceptions\InputException;
 use Firehed\Input\Interfaces\ParserInterface;
 
+use function is_array;
+use function json_decode;
+use function json_last_error;
+use function strlen;
+
+use const JSON_THROW_ON_ERROR;
+
 class JSON implements ParserInterface, DecoderInterface
 {
     public function decode(string $rawInput): mixed
     {
+        if ($rawInput === '') {
+            return null; // Maybe this should be a parse error?
+        }
+        $decoded = json_decode($rawInput);
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            throw new InputException(InputException::PARSE_ERROR);
+        }
+        return $decoded;
     }
 
+    /**
+     * Legacy behavior always does a json_decode to an associative array. This
+     * is a lossy conversion from certain kinds of raw inputs due to PHP array
+     * semantics.
+     */
     public function parse(string $raw_input): array
     {
         if (!strlen($raw_input)) {
